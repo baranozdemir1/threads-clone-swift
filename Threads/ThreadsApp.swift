64d1da9869d5031aa3e13237c8280e150cd01f00
Baran Özdemir <baranozdemir305@gmail.com>
2023-07-07 03:44:09 +0300

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Baran Özdemir on 7.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
